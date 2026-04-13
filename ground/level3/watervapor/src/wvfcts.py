corrected rhov uncertainty calculation using Hogan et al 2005 for Ze precision calculation; Battaglia+Kollas 2019 Eq 7 / Roy et al 2018 Eq 13 for rhov uncertainty.

ground/level3/watervapor/src/wvfcts.py
@@ -26,6 +26,7 @@ def create_wv_dataset(dsvars, attrs):
     'rhov' : (['time','height'], dsvars['rhov'], {'units': 'g m-3', 'long_name':'absolute humidity profile retrieved from DAR measurements'}),
     'diffkappa' : (['time','height'], dsvars['diffkappa'], {'units': 'mm6 m-3', 'long_name':'radar reflectivity Ze'}),
     'diffgamma' : (['time','height'], dsvars['diffgamma'], {'units': 'm s-1', 'long_name':'radar mean Doppler Velocity'}),
+    'deltarhov' : (['time','height'], dsvars['deltarhov'], {'units': 'g m-3', 'long_name':'rhov uncertainty'}),
     'nranges' : (['height'], dsvars['nranges'], {'units': '-', 'long_name':'number of range bins that fit into R spacing'})
         }
     
@@ -125,8 +126,19 @@ def get_diffgamma(R, inradar, outall=False):
 
         #calculate second term with deltaZe at r and R for uncertainty calculation: (whole second term of Eqn 7 in Battaglia and Kollias 2019)
         ## note that no deltaZe can be calculated for 175 so far (SW missing in software output), thus assume the same as in 167.
-        deltaZerR.append( np.sqrt( 2 * radar.deltaZe.values[:,i]**2  + 2 * radar.deltaZe.values[:,resiterated-1]**2))
+        
+        #this one is for deltaZe in dB using Battaglia and Kollias Eq (7) - unclear in terms of units!!!
+        #deltaZerR.append( np.sqrt( 2 * radar.deltaZe.values[:,i]**2  + 2 * radar.deltaZe.values[:,resiterated-1]**2))
+        
+        #this one is the second term of Roy et al 2018 Eq (13)
+        #convert deltaZe (in dB) to linear mm6/m3 by dividing with 4.343 factor
+        sigmarhovterm2 =  np.sqrt( 
+            ((radar.deltaZe.values[:,i]/4.343) / radar.GZe.values[:,i] ) **2 + #deltaZe at r1 for 167
+            ((radar.deltaZe.values[:,i]/4.343) / radar.G2Ze.values[:,i] ) **2 + #assume same deltaZe for 174 at r1
+            ((radar.deltaZe.values[:,resiterated-1]/4.343) / radar.GZe.values[:,resiterated-1] ) **2 + #deltaZe at r2 for 167
+            ((radar.deltaZe.values[:,resiterated-1]/4.343) / radar.G2Ze.values[:,resiterated-1] ) **2 ) #assume same deltaZe for 174 at r2
 
+        deltaZerR.append(sigmarhovterm2)
         
     gamma1 = np.asarray(gamma1).T
     gamma2 = np.asarray(gamma2).T

ground/level3/watervapor/wv_retrieval.py
@@ -3,6 +3,8 @@ import xarray as xr
 import datetime as dt
 
 from level3.watervapor.src import wvfcts as wvfct
+import importlib as imp
+imp.reload(wvfct)
 
 import pandas as pd
 
@@ -47,28 +49,29 @@ def run_retrieval_ground(radar, thermo, lut, info, write=False):
         #calculate profile based on differential gamma, and differential kappa
         wvprof = np.divide(diffgamma, diffkappa)*1000.  #diffgamma units: 1/m ; diffkappa: m**2 kg**-1 ; wvprof: g m**(-3)
         
-        #calculate rhov uncertainty according to Battaglia and Kollias 2019 Eq 7; Roy et al 2018 Eqn 9
+        #calculate rhov uncertainty according to Battaglia and Kollias 2019 Eq 7; Roy et al 2018 Eqn 13
         deltarhov = 1/(2 * R * diffkappa) * deltaZerR * 1000. #convert to g m-3
+        
 
         
         #prepare variables to be stored in dataset:
-        exportvars = [diffkappa, diffgamma, nvolumes, wvprof, R, radar.time.values, radar.height.values]
-        varnames = ['diffkappa', 'diffgamma', 'nranges', 'rhov', 'R', 'time', 'height']
+        exportvars = [diffkappa, diffgamma, nvolumes, wvprof, R, radar.time.values, radar.height.values, deltarhov]
+        varnames = ['diffkappa', 'diffgamma', 'nranges', 'rhov', 'R', 'time', 'height', 'deltarhov']
         tods = {}
         for vv,var in enumerate(exportvars):
             tods[varnames[vv]] = var
-        
+
         #create dataset:
         attrs = radar.attrs.copy()
         wvxrds = wvfct.create_wv_dataset(tods, attrs)
-        
+
         #filter obtained profiles by how many range gates were averaged together; ie set all profile output to nan where nranges is smaller than minnranges given in info json
         wvxrds.rhov.values[:,np.where(wvxrds.nranges < int(info['watervaporsettings']['minNranges']))] = np.nan
-        
+
 
         #interpolate in height and time and smooth:
         print('resampling on %s time interval by taking mean of window...'%info['watervaporsettings']['tavg'])
-        
+
         wvxrdssmooth = wvxrds.resample(time='%s'%info['watervaporsettings']['tavg'],closed='right').mean() #put timestamp at end of averaging period to be consistent with radar time stamping (rpg puts timestamp at end of one measurement)
         
         print('smoothing output vertically on R=%i by using rolling mean'%R)
@@ -78,14 +81,14 @@ def run_retrieval_ground(radar, thermo, lut, info, write=False):
         #print('interpolating smoothing output to regular grid with R')
         #Rgrid = np.arange(0, 11000, R)
         #wvxrdssmooth = wvxrdssmooth.interp( height = Rgrid)
-
+        1/0
 
         #store to netcdf
         if write==True:
             
             #output of original profiles:
-            #ncfile = info['global']['mission'] +'_%s_level3b_watervapor_%s%s%s_R%i_tavg%s_original.nc'%(info['global']['version'], info['yyyy'], info['mm'], info['dd'], R, info['watervaporsettings']['tavg'])
-            #wvxrds.to_netcdf(info['paths']['output'] + ncfile)
+            ncfile = info['global']['mission'] +'_%s_level3b_watervapor_%s%s%s_R%i_tavg%s_original.nc'%(info['global']['version'], info['yyyy'], info['mm'], info['dd'], R, info['watervaporsettings']['tavg'])
+            wvxrds.to_netcdf(info['paths']['output'] + ncfile)
             
             #output of smoothed profiles:
             ncfile = info['global']['mission'] +'_%s_level3b_watervapor_%s%s%s_R%i_tavg%s.nc'%(info['global']['version'], info['yyyy'], info['mm'], info['dd'], R, info['watervaporsettings']['tavg'])