fixed bug in delta rhov calculation; convert deltaZe to linear units in formula

ground/level3/watervapor/src/wvfcts.py
@@ -131,14 +131,22 @@ def get_diffgamma(R, inradar, outall=False):
         #this one is for deltaZe in dB using Battaglia and Kollias Eq (7) - unclear in terms of units!!!
         #deltaZerR.append( np.sqrt( 2 * radar.deltaZe.values[:,i]**2  + 2 * radar.deltaZe.values[:,resiterated-1]**2))
         
+        #======= used previously but wrong!! =====
         #this one is the second term of Roy et al 2018 Eq (13)
         #convert deltaZe (in dB) to linear mm6/m3 by dividing with 4.343 factor
+        #sigmarhovterm2 =  np.sqrt( 
+        #    ((radar.deltaZe.values[:,i]/4.343) / radar.GZe.values[:,i] ) **2 + #deltaZe at r1 for 167
+        #    ((radar.deltaZe.values[:,i]/4.343) / radar.G2Ze.values[:,i] ) **2 + #assume same deltaZe for 174 at r1
+        #    ((radar.deltaZe.values[:,resiterated-1]/4.343) / radar.GZe.values[:,resiterated-1] ) **2 + #deltaZe at r2 for 167
+        #    ((radar.deltaZe.values[:,resiterated-1]/4.343) / radar.G2Ze.values[:,resiterated-1] ) **2 ) #assume same deltaZe for 174 at r2
+        #========
+        deltaZelin = helpfct.get_zlin(radar.deltaZe.values)
+        #now both deltaZe and radar.Ze should be in linear units.
         sigmarhovterm2 =  np.sqrt( 
-            ((radar.deltaZe.values[:,i]/4.343) / radar.GZe.values[:,i] ) **2 + #deltaZe at r1 for 167
-            ((radar.deltaZe.values[:,i]/4.343) / radar.G2Ze.values[:,i] ) **2 + #assume same deltaZe for 174 at r1
-            ((radar.deltaZe.values[:,resiterated-1]/4.343) / radar.GZe.values[:,resiterated-1] ) **2 + #deltaZe at r2 for 167
-            ((radar.deltaZe.values[:,resiterated-1]/4.343) / radar.G2Ze.values[:,resiterated-1] ) **2 ) #assume same deltaZe for 174 at r2
-
+            ((deltaZelin[:,i]) / radar.GZe.values[:,i] ) **2 + #deltaZe at r1 for 167
+            ((deltaZelin[:,i]) / radar.G2Ze.values[:,i] ) **2 + #assume same deltaZe for 174 at r1
+            ((deltaZelin[:,resiterated-1]) / radar.GZe.values[:,resiterated-1] ) **2 + #deltaZe at r2 for 167
+            ((deltaZelin[:,resiterated-1]) / radar.G2Ze.values[:,resiterated-1] ) **2 ) #assume same deltaZe for 174 at r2
         deltaZerR.append(sigmarhovterm2)
         
     gamma1 = np.asarray(gamma1).T